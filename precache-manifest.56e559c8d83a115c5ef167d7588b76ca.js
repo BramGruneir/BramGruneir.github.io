08352d8639fa3fdf81d0f6c9b86bbf4b04fe0f8b
Bram Gruneir <bram@cockroachlabs.com>
2020-10-19 10:43:05 -0400

    small update to readme and adding crdb version qualifier

---
+++ precache-manifest.56e559c8d83a115c5ef167d7588b76ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf01e715ae632b828ed50da68eaf8e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a53892fdee19400778b",
+    "url": "/static/css/main.4c91e4f9.chunk.css"
+  },
+  {
+    "revision": "63c5a80ee2d52d612977",
+    "url": "/static/js/2.52988e62.chunk.js"
+  },
+  {
+    "revision": "29f472274943c3f355e5582c46742fd1",
+    "url": "/static/js/2.52988e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a53892fdee19400778b",
+    "url": "/static/js/main.5c354b60.chunk.js"
+  },
+  {
+    "revision": "7de42078b975fb280c0f",
+    "url": "/static/js/runtime-main.64ad6046.js"
+  }
+]);
